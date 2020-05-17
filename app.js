bb0ed3e49d50ff05e18f6871e7373beb012763eb
acolistro <arcolistro@gmail.com>
2020-05-17 09:31:09 -0700

    practice using the require function with multiple modules and objects

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
+//Playing with the require function and objects
+var greet = require('./greet1');
+greet();
+
+var greet2 = require('./greet2').greet;
+greet2();
+
+var greet3 = require('./greet3');
+greet3.greet();
+
+greet3.greeting = 'Changed hello world';
+
+var greet3b = require('./greet3');
+greet3b.greet();
+
+
 //practice with require() function
-var greet = require('./greet');
-greet.english(); 
-greet.spanish(); 
-
-
-
-
+// var greet = require('./greet');
+// greet.english(); 
+// greet.spanish(); 
 
 //practice with IIFEs
 // (function (lastName) {

4bb6767df3498b0290210e27cd6ee863806ac060
acolistro <arcolistro@gmail.com>
2020-05-17 08:24:22 -0700

    practice with scoping and IIFEs

--- app.js
+++ app.js
@@ -1,21 +1,34 @@
+//practice with IIFEs
+(function (lastName) {
+  
+  var firstName = 'John';
+  console.log(firstName);
+  console.log(lastName);
+
+}('Doe'));
+
+var firstName = 'Jane';
+console.log(firstName);
+
+//practice with require() function
 // var greet = require('./greet.js');
 // greet();
 
 // pass by value
-function change(b) {
-  b = 2;
-}
+// function change(b) {
+//   b = 2;
+// }
 
-var a = 1;
-change(a);
-console.log(a);
+// var a = 1;
+// change(a);
+// console.log(a);
 
-function changeObj(d) {
-  d.prop1 = function() {};
-  d.prop2 = {};
-}
+// function changeObj(d) {
+//   d.prop1 = function() {};
+//   d.prop2 = {};
+// }
 
-var c = {};
-c.prop1 = {};
-changeObj(c);
-console.log(c);
+// var c = {};
+// c.prop1 = {};
+// changeObj(c);
+// console.log(c);
